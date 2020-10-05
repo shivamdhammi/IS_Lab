1aff6f71a2e4eb75145ad90f84bf161e09f07ded
Shivam Dhammi <dhammishivam@gmail.com>
2020-10-05 12:06:06 +0530

    more exp

---
+++ 11720013_LAB_2.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	cout<<"Enter the plain text : ";
+	string s;
+	getline(cin,s);
+	
+	int key = 40;
+	
+	for(int i=0;i<s.length();i++){
+		s[i] = (s[i] - i - key +128)%128;
+	}
+	cout<<"Encrypted text : ";
+	cout<<s<<endl;
+	
+	for(int i=0;i<s.length();i++){
+		s[i] = (s[i] + i + key +128)%128;
+	}
+	cout<<"Decrypted text : ";
+	cout<<s;
+	return 0;
+}
---
+++ 11720013_modified_ceasar.cpp
@@ -0,0 +1,30 @@
+#include <iostream> 
+using namespace std; 
+  
+string encrypt(string &text, int s){ 
+    for (int i=0;i<text.length();i++) { 
+    	s=(s*17+17)%128;
+        text[i] = (text[i]+s+128)%128;
+    } 
+    return text; 
+}
+string dencrypt(string &text, int s){ 
+    for (int i=0;i<text.length();i++) { 
+    	s=(s*17+17)%128;
+        text[i] = (text[i]-s+128)%128;
+    }
+    return text; 
+}  
+  
+int main() 
+{ 
+	cout<<"Enter the plain text : ";
+	string text;
+	getline(cin,text);
+    int shift; 
+    cout<<"Enter the key : ";
+    cin>>shift;
+    cout << "\nEncrypted text : " << encrypt(text, shift); 
+    cout << "\nDencrypted text : " << dencrypt(text, shift); 
+    return 0; 
+} 
